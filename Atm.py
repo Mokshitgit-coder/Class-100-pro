4b787a635f84b542955cbb176e58c0ec79f8bbdc
Mokshitgit-coder <72385298+Mokshitgit-coder@users.noreply.github.com>
2022-05-20 13:10:15 +0530

    Add files via upload

---
+++ Atm.py
@@ -0,0 +1,10 @@
+class atm (object):
+    def __init__(self,card,pin):
+        self.card=card
+        self.pin=pin
+
+    def CashWithdrawl(self,cashWithdawing):
+        print ("Enter the amont you want to take")
+    def BalanceEnquiry(self,BalanceEnquiry):
+        print ("Check the balance here")
+atm()
